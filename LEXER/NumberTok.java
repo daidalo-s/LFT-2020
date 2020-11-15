ef0ced2e5e962c9128be46dee686850549728725
averageyimain <69860137+averageyimain@users.noreply.github.com>
2020-11-15 22:39:26 +0100

    Delete NumberTok.java

--- LEXER/NumberTok.java
+++
@@ -1,6 +0,0 @@
-public class NumberTok extends Token {
-	// ... completare ...
-	public String number = "";
-    public NumberTok(int t , String n) { super(t); number=n; }
-    public String toString() { return "<256, " + number + ">";}
-}

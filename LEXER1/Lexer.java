893cb4c896fa82f4d14f0d4e4b64ea4fbaca6b23
Lorenzo <lorenzo@Argos.homenet.telecomitalia.it>
2021-02-21 23:41:09 +0100

    Ultime modifiche

--- LEXER1/Lexer.java
+++ LEXER1/Lexer.java
@@ -183,7 +183,7 @@ public class Lexer {
 		
     public static void main(String[] args) {
         Lexer lex = new Lexer();
-        String path = "/Users/lorenzo/Projects /LFT-2020/LEXER/test.txt"; 
+        String path = "/Users/lorenzo/Projects /LFT-2020/LEXER1/test.txt"; 
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             Token tok;
